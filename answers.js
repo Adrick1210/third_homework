64f775862ce216e5a8d1be5f268bf2ea1b332f75
Christian Alexander <christianalexander@christians-mbp.lan>
2023-04-29 16:06:32 -0700

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -11,3 +11,17 @@
 // for (let i = 0; i < 202; i += 2) {
 //     console.log(i)
 // }
+
+// fizzBuzz
+
+for (let i = 1; i < 100; i++) {
+  if (i % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
