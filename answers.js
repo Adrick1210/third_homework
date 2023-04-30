4bcd888628161790d21f9b16e7dbc97fd73dd12d
Christian Alexander <christianalexander@christians-mbp.lan>
2023-04-29 22:30:42 -0700

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -36,19 +36,42 @@
 
 // Wild Wild Life
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant", 5000, "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
-plantee.splice(2, 1, 5001);
+// plantee.splice(2, 1, 5001);
 
-wolfy.splice(3, 1, "Gotham City");
-wolfy.splice(0, 1, "gameboy");
+// wolfy.splice(3, 1, "Gotham City");
+// wolfy.splice(0, 1, "gameboy");
 
-dart.push("Hawkins");
+// dart.push("Hawkins");
 
-console.log(wolfy, sharky, plantee, porgee, dart);
+// console.log(wolfy, sharky, plantee, porgee, dart);
+
+// =======================================================================
+
+// Yell at the ninja turtles
+
+// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+// const yellTurtle = [];
+
+// for (let name of ninjaTurtles) {
+//    const upper = name.toUpperCase();
+//    yellTurtle.push(upper);
+// }
+// console.log(yellTurtle);
+
+// For this exercise I researched stack overflow for the answer I was
+// looking for. I found this method and decided to decipher and try it out.
+// In my solution, I took my original array, and also created a empty array
+// of yellTurtle. (Had to refresh on for of loops) I designated name as the 
+// value of each item in the original array. I created a new variable of 
+// upper that invokes the toUpperCase(). By doing so, I took the items from
+// ninjaTurtle and converted them to uppercase and placed them inside of the
+// new array yellTurtle. (Needed a place for my thoughts and hope I was on the 
+// correct thought process for this solution)
 
 // =======================================================================
